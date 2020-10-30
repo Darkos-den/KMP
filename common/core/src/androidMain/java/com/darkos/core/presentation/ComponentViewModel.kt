43b0236cd4f3c4c1a22853392fd802802ea689bc
Denis <loboda.deni@gmail.com>
2020-10-30 14:56:46 +0200

    fix

--- common/core/src/androidMain/java/com/darkos/core/presentation/ComponentViewModel.kt
+++ common/core/src/androidMain/java/com/darkos/core/presentation/ComponentViewModel.kt
@@ -6,9 +6,11 @@ import com.darkos.mvu.EffectHandler
 import com.darkos.mvu.Reducer
 import com.darkos.mvu.models.MVUState
 import com.darkos.mvu_program.Program
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
-abstract class ComponentViewModel<T : MVUState, EH: EffectHandler, R: Reducer<T>>(
+abstract class ComponentViewModel<T : MVUState, EH : EffectHandler, R : Reducer<T>>(
     effectHandler: EH,
     reducer: R
 ) : BaseViewModelImpl(), FlowComponent<T> {
@@ -16,13 +18,20 @@ abstract class ComponentViewModel<T : MVUState, EH: EffectHandler, R: Reducer<T>
     abstract override val initial: T
     override val state = MutableStateFlow(initial)
 
-    protected val program = Program(
-        initialState = initial,
-        component = this,
-        effectHandler = effectHandler,
-        reducer = reducer
-    ).also {
-        it.start()
+    protected val program: Program<T> by lazy {
+        Program(
+            initialState = initial,
+            component = this,
+            effectHandler = effectHandler,
+            reducer = reducer
+        )
+    }
+
+    init {
+        ioScope.launch {
+            delay(200)
+            program.start()
+        }
     }
 
     override fun onCleared() {
